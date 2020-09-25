eed58803202dde8717fd845d60330e750b700097
Ermeydan-coder <musaermeydan@gmail.com>
2020-09-25 23:59:15 +0200

    Find the Largest Number

---
+++ python/coding-challenges/cc-003-find-the-largest-number/find-the- largest-nummer.py
@@ -0,0 +1,11 @@
+def max_num_in_list( list ):
+    max = list[ 0 ]
+    for a in list:
+        if a > max:
+            max = a
+    return max
+print(max_num_in_list([1, 2, 8, 0]))
+
+def my_max(*x):
+    return max(x)
+print(my_max(-3,-5,-7))
